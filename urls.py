f2cd83099a23035b41c4687d6d8191a2f4df9eec
sima59 <simabageri@gmail.com>
2020-11-18 09:27:42 -0600

    Just Mysite

--- urls.py
+++ urls.py
@@ -13,19 +13,31 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.sitemaps.views import sitemap
 from blog.sitemaps import PostSitemap
 
 sitemaps = {
-'posts': PostSitemap,
+    'posts': PostSitemap,
 }
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls', namespace='blog')),
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
-name='django.contrib.sitemaps.views.sitemap')
+    path('cart/', include('cart.urls', namespace='cart')),
+    path('shop/', include('shop.urls', namespace='shop')),
+    path( '',include('blog.urls', namespace='blog')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},name='django.contrib.sitemaps.views.sitemap')
+    
     
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, 
+                          document_root=settings.STATIC_ROOT)
